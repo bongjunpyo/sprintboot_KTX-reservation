0eaceb78c5669c8259d2b173817aa4af7290d8cf
bongjunpyo <wnsvy030@naver.com>
2023-11-12 23:26:05 +0900

    이것도 끄적

---
+++ src/main/resources/templates/LoginAndSignup.js
@@ -0,0 +1,16 @@
+function login() {
+
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+
+
+    console.log('Login clicked. Username:', username, 'Password:', password);
+}
+
+function signup() {
+
+    var signupUsername = document.getElementById('signup-username').value;
+    var signupPassword = document.getElementById('signup-password').value;
+
+    console.log('Sign Up clicked. Username:', signupUsername, 'Password:', signupPassword);
+}
