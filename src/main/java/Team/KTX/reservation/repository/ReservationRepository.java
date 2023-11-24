5efdfa176865f0e8f76f6e856529419d0f683588
bongjunpyo <wnsvy030@naver.com>
2023-11-24 17:10:50 +0900

    loginedmain을 reservation으로 변경

--- src/main/java/Team/KTX/reservation/repository/ReservationRepository.java
+++ src/main/java/Team/KTX/reservation/repository/ReservationRepository.java
@@ -5,15 +5,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
 
-    // 특정 열차 번호에 대한 예약 조회
+
     public Reservation findByTrainNumber(int trainNumber);
 
-    // 출발지와 목적지에 대한 예약 조회
+
     public Reservation findByDepartureLocationAndDestinationLocation(String departureLocation, String destinationLocation);
 
-    // 출발 시간과 도착 시간에 대한 예약 조회
+
     public Reservation findByDepartureTimeAndArrivalTime(String departureTime, String arrivalTime);
 
-    // 추가적인 필요에 따라 다양한 메서드 추가 가능
+
 
 }
