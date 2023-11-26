9327648c1e896850a482e1ce9aac4b425fe9b1b5
jinwon1025 <jinwon2812@naver.com>
2023-11-27 00:58:10 +0900

    날짜 들어가게

--- src/main/java/Team/KTX/reservation/domain/Reservation.java
+++ src/main/java/Team/KTX/reservation/domain/Reservation.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -14,6 +15,7 @@ import java.time.format.DateTimeFormatter;
 @Table(name = "reservations")
 @NoArgsConstructor
 @AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class Reservation {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
