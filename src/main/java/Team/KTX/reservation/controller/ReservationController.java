1668e015fd6752f28514d9f115877ded399c0e6f
jinwon1025 <jinwon2812@naver.com>
2023-12-07 14:14:05 +0900

    Seat 테이블 + 관련 코드 삭제

--- src/main/java/Team/KTX/reservation/controller/ReservationController.java
+++ src/main/java/Team/KTX/reservation/controller/ReservationController.java
@@ -1,18 +1,13 @@
 package Team.KTX.reservation.controller;
 
 import Team.KTX.reservation.domain.Reservation;
-import Team.KTX.reservation.domain.Seat;
 import Team.KTX.reservation.dto.AddReservationRequest;
-import Team.KTX.reservation.dto.AddSeatRequest;
 import Team.KTX.reservation.dto.ReservationResponse;
 import Team.KTX.reservation.dto.UpdateReservationRequest;
 import Team.KTX.reservation.service.ReservationService;
-import Team.KTX.reservation.service.SeatService;
-import jakarta.persistence.SqlResultSetMapping;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,9 +18,7 @@ public class ReservationController {
 
     @Autowired
     private ReservationService reservationService;
-
-    @Autowired
-    private SeatService seatService;
+    
 
 
     @PostMapping("/api/reservations")
@@ -55,11 +48,6 @@ public class ReservationController {
         return ResponseEntity.ok().body(userReservations);
     }
 
-    //@GetMapping("/api/reservations/{rid}")
-    //public ResponseEntity<Reservation> findReservation(@PathVariable long rid){
-     //   Reservation reservation = reservationService.findOne(rid);
-       // return ResponseEntity.ok().body(reservation);
-    //}
 
     @DeleteMapping("/api/reservations/{id}")
     public ResponseEntity<String> deleteReservation(@PathVariable long id){
--- src/main/java/Team/KTX/reservation/repository/SeatRepository.java
+++
@@ -1,7 +0,0 @@
-package Team.KTX.reservation.repository;
-
-import Team.KTX.reservation.domain.Seat;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SeatRepository extends JpaRepository<Seat, Long> {
-}
